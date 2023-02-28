9ecd43783b4583cf2ebf250e29721865f7bbaf5b
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-02-27 21:14:15 -0600

    Added text to ReadMe and changed query selection

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## How to properly use ```.querySelectorAll()```
 
+Let's say that you want to make a project in which you use lots of the same
+repeated element, and you want to add an event listener to all the elements
--- assets/script/index.js
+++ assets/script/index.js
@@ -14,22 +14,9 @@ const numInp = document.querySelector('.num-inp');
 
 // Buttons
 const clearBtn = document.querySelector('.g-span-3');
-const oneBtn = document.querySelector('#one');
-const twoBtn = document.querySelector('#two');
-const threeBtn = document.querySelector('#three');
-const fourBtn = document.querySelector('#four');
-const fiveBtn = document.querySelector('#five');
-const sixBtn = document.querySelector('#six');
-const sevenBtn = document.querySelector('#seven');
-const eightBtn = document.querySelector('#eight');
-const nineBtn = document.querySelector('#nine');
-const zeroBtn = document.querySelector('#zero');
-const dotBtn = document.querySelector('#dot');
-const divisBtn = document.querySelector('#divis');
-const timesBtn = document.querySelector('#times');
-const subBtn = document.querySelector('#sub');
-const addBtn = document.querySelector('#add');
 const equalsBtn = document.querySelector('#equals');
+
+const inpBtns = document.querySelectorAll("input[type='button']")
 
 // ---------------------- Calculator Functions ----------------------
 
@@ -76,62 +63,6 @@ clearBtn.addEventListener('click', function() {
     pastInp.innerText = '';
 })
 
-oneBtn.addEventListener('click', function() {
-    numInp.value += '1';
-});
-
-twoBtn.addEventListener('click', function() {
-    numInp.value += '2';
-});
-
-threeBtn.addEventListener('click', function() {
-    numInp.value += '3';
-});
-
-fourBtn.addEventListener('click', function() {
-    numInp.value += '4';
-});
-
-fiveBtn.addEventListener('click', function() {
-    numInp.value += '5';
-});
-
-sixBtn.addEventListener('click', function() {
-    numInp.value += '6';
-});
-
-sevenBtn.addEventListener('click', function() {
-    numInp.value += '7';
-});
-
-eightBtn.addEventListener('click', function() {
-    numInp.value += '8';
-});
-
-nineBtn.addEventListener('click', function() {
-    numInp.value += '9';
-});
-
-zeroBtn.addEventListener('click', function() {
-    numInp.value += '0';
-});
-
-dotBtn.addEventListener('click', function() {
-    numInp.value += '.';
-});
-
-divisBtn.addEventListener('click', function() {
-    numInp.value += ' / ';
-});
-
-timesBtn.addEventListener('click', function() {
-    numInp.value += ' * ';
-});
-
-subBtn.addEventListener('click', function() {
-    numInp.value += ' - ';
-});
-
-addBtn.addEventListener('click', function() {
-    numInp.value += ' + ';
-});
+inpBtns.forEach(inpBtns => inpBtns.addEventListener('click', function() {
+    numInp.value += inpBtns.value;
+}))
